67aa1766a0974d417ef08f8e00625c3b0563b74d
Andreas Lien Olsen <andreas.olsen@gmail.com>
2014-01-08 20:45:57 +0100

    Added simple ASCII display

--- BerlinClockLib/ShowMe/Program.cs
+++ BerlinClockLib/ShowMe/Program.cs
@@ -1,46 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 using BerlinClockLib;
 
 namespace ShowMe
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
-            
             while (true)
             {
                 var mengelHeur = DateTime.Now.ToString("HH:mm:ss").toMengelHeur();
-                foreach (var c in mengelHeur)
-                {
-                    if (c == 'Y')
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write(c);
-                    }
-                    else if (c == 'R')
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(c);
-                    }
-                    else if (c == 'O')
-                    {
-                        Console.ForegroundColor = ConsoleColor.Black ;
-                        Console.Write(c);
-                    }
-                    else if(c == '\r')
-                        Console.WriteLine();
-                }
-                
-                Console.WriteLine();
+                Ascii(mengelHeur);
                 System.Threading.Thread.Sleep(1000);
             }
+        }
+
+        public static void Ascii(string clock)
+        {
+
+            //                  222222222                  
+            //|===========================================|
+            //|1111111111|2222222222|3333333333|4444444444|
+            //|===========================================|
+            //|1111111111|2222222222|3333333333|4444444444|
+            //|===========================================|
+            //|11a|a22|3a3|4a4|5a5|6aq|7aq|8aq|9aq|0aq|1aq|
+            //|===========================================|
+            //|1111111111|2222222222|3333333333|4444444444|
+            Console.Clear();
+
+            var lines = clock.Replace(Environment.NewLine, ":").Split(':');
+
+            Seconds(lines[0]);
+            Console.WriteLine("|===========================================|");
+            Hours(lines[1]);
+            Console.WriteLine("|===========================================|");
+            Hours(lines[2]);
+            Console.WriteLine("|===========================================|");
+            Minutes(lines[3]);
+            Console.WriteLine("|===========================================|");
+            Hours(lines[4]);
+            Console.WriteLine("|===========================================|");
+        }
+
+        private static void Hours(string quarters)
+        {
+            foreach (var c in quarters.ToCharArray())
+            {
+                Pipe();
+                Writer.Write(c == 'R' ? ConsoleColor.Red : ConsoleColor.Black, "          ");
+            }
+            Pipe();
+            Console.WriteLine();
+        }
+
+        private static void Minutes(string minutes)
+        {
+            foreach (var c in minutes.ToCharArray())
+            {
+                Pipe();
+                var color = ConsoleColor.Black;
+                if (c == 'R')
+                    color = ConsoleColor.Red;
+                if (c == 'Y')
+                    color = ConsoleColor.Yellow;
+                Writer.Write(color, "   ");
+            }
+            Pipe();
+            Console.WriteLine();
+        }
+
+        private static void Seconds(string seconds)
+        {
+            Writer.Write(ConsoleColor.Black, "               ");
+            Pipe();
+            Writer.Write(seconds == "Y" ? ConsoleColor.Yellow : ConsoleColor.Black, "             ");
+            Pipe();
+            Writer.Write(ConsoleColor.Black, "               ");
+            Console.WriteLine();
+        }
+
+        private static void Pipe()
+        {
+            Console.Write("|");
+        }
+    }
+
+    public static class Writer
+    {
+        public static void Write(ConsoleColor color, string input)
+        {
+            var backgroundColor = Console.BackgroundColor;
+            var foregroundColor = Console.ForegroundColor;
+            Console.BackgroundColor = color;
+            Console.ForegroundColor = color;
+            Console.Write(input);
+            Console.BackgroundColor = backgroundColor;
+            Console.ForegroundColor = foregroundColor;
         }
     }
 }
